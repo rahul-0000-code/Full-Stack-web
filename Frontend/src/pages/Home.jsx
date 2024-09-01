c3536ae6c05ceb6faf46d8bedaa2a0083785b4d6
rahul-0000-code <rahulkbhandari272002@gmail.com>
2024-09-01 16:30:36 +0530

    Assets and logo's for frontend

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div> 
+            {/* Section1 */}
+            <div>
+                <Link to={"/signup"}>
+                    <div>
+                        <div>
+                            <p>Become an Instructor</p>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+            {/* Section2 */}
+
+            {/* Section3 */}
+
+            {/* Footer */}
+        </div>
+    )
+}
+
+export default Home;
